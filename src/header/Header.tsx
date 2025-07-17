2b3ae13347250627558c093114eb08f92dfe2239
Seopia <wjdwltjq7289@gmail.com>
2025-07-17 17:01:23 +0900

    init

---
+++ src/header/Header.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import s from './Header.module.css';
+const Header = () => {
+    const [cameraState, setCameraState] = useState(true);
+    return (
+        <header className={s.container}>
+            <div className={s.titleContainer}>
+                <span>아이콘</span>
+                <div>
+                    <h1>헬멧 감지 시스템</h1>
+                    <div>실시간 오토바이 헬멧 착용 모니터링</div>
+                </div>
+            </div>
+            <div className={s.status}>
+                <div style={{background:cameraState?'green':'red'}} className={s.state}></div>
+                <div>시스템 정상</div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
